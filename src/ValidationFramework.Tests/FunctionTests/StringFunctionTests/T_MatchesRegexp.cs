61bae2e3b9a97ff2a776a55d19f75c3524f5411c
André Kirst <andre.kirst@comparex.de>
2017-12-07 13:36:21 +0100

    #5 - MatchesRegexp hinzugefügt

---
+++ src/ValidationFramework.Tests/FunctionTests/StringFunctionTests/T_MatchesRegexp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ValidationFramework.Functions;
+
+namespace ValidationFramework.Tests.FunctionTests.StringFunctionTests
+{
+    public class T_MatchesRegexp
+    {
+        [Theory]
+        [InlineData("a", "a", true)]
+        [InlineData("0", "a", false)]
+        [Trait(name: "Issue", value: "5")]
+        [Trait(name: "Function", value: "MatchesRegexp")]
+        public void TestMatchesRegexpData(string input, string pattern, bool expectedValue)
+        {
+            bool actual = input.MatchesRegexp(pattern: pattern);
+
+            Assert.Equal(expected: expectedValue, actual: actual);
+        }
+
+        [Fact]
+        [Trait(name: "Issue", value: "5")]
+        [Trait(name: "Function", value: "MatchesRegexp")]
+        public void TestMatchesRegexpValueIsNullExpectArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(testCode: () =>
+            {
+                string s = null;
+                bool test = s.MatchesRegexp(pattern: "a-z");
+            });
+        }
+
+        [Fact]
+        [Trait(name: "Issue", value: "5")]
+        [Trait(name: "Function", value: "MatchesRegexp")]
+        public void TestMatchesRegexpPatternIsNullExpectArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(testCode: () =>
+            {
+                string s = "a";
+                bool test = s.MatchesRegexp(pattern: null);
+            });
+        }
+    }
+}
---
+++ src/ValidationFramework/Functions/StringFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidationFramework.Functions
+{
+    public static class StringFunctions
+    {
+        public static bool MatchesRegexp(this string value, string pattern)
+        {
+            return Regex.IsMatch(
+                input: value,
+                pattern: pattern);
+        }
+    }
+}
