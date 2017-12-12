2b5a9e2e67745bad14cf71ca0d72b3376f7d268f
André Kirst <github@andrekirst.de>
2017-12-12 13:05:19 +0100

    #5 - Implementierung Between

---
+++ src/ValidationFramework.Tests/FunctionTests/RangeFunctionTests/T_Between.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ValidationFramework.Functions;
+
+namespace ValidationFramework.Tests.FunctionTests.RangeFunctionTests
+{
+    public class T_Between
+    {
+        [Theory(DisplayName = "TestBetweenDouble")]
+        [Trait(name: "Function", value: "Between")]
+        [Trait(name: "Issue", value: "5")]
+        [InlineData(3, 2, 4, true)]
+        [InlineData(5, 2, 4, false)]
+        public void TestBetweenDouble(double input, double start, double end, bool expectedValue)
+        {
+            bool actual = input.Between(start: start, end: end);
+
+            Assert.Equal(expected: expectedValue, actual: actual);
+        }
+
+        [Theory(DisplayName = "TestBetweenFloat")]
+        [Trait(name: "Function", value: "Between")]
+        [Trait(name: "Issue", value: "5")]
+        [InlineData(3, 2, 4, true)]
+        [InlineData(5, 2, 4, false)]
+        public void TestBetweenFloat(float input, float start, float end, bool expectedValue)
+        {
+            bool actual = input.Between(start: start, end: end);
+
+            Assert.Equal(expected: expectedValue, actual: actual);
+        }
+
+        [Theory(DisplayName = "TestBetweenInt")]
+        [Trait(name: "Function", value: "Between")]
+        [Trait(name: "Issue", value: "5")]
+        [InlineData(3, 2, 4, true)]
+        [InlineData(5, 2, 4, false)]
+        public void TestBetweenInt(int input, int start, int end, bool expectedValue)
+        {
+            bool actual = input.Between(start: start, end: end);
+
+            Assert.Equal(expected: expectedValue, actual: actual);
+        }
+
+        [Theory(DisplayName = "TestBetweenDateTime")]
+        [Trait(name: "Function", value: "Between")]
+        [Trait(name: "Issue", value: "5")]
+        [MemberData(nameof(TestBetweenDateTimeData))]
+        public void TestBetweenDateTime(DateTime input, DateTime start, DateTime end, bool expectedValue)
+        {
+            bool actual = input.Between(start: start, end: end);
+
+            Assert.Equal(expected: expectedValue, actual: actual);
+        }
+
+        public static IEnumerable<object[]> TestBetweenDateTimeData()
+        {
+            return new List<object[]>()
+            {
+                new object[] { new DateTime(2017, 12, 12), new DateTime(2017, 1, 1), new DateTime(2017, 12, 31), true },
+                new object[] { new DateTime(2017, 12, 12), new DateTime(2017, 1, 1), new DateTime(2017, 11, 11), false }
+            };
+        }
+    }
+}
---
+++ src/ValidationFramework/Functions/RangeFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationFramework.Functions
+{
+    public static class RangeFunctions
+    {
+        public static bool Between(this double value, double start, double end)
+        {
+            return value >= start && value <= end;
+        }
+
+        public static bool Between(this float value, float start, float end)
+        {
+            return value >= start && value <= end;
+        }
+
+        public static bool Between(this int value, int start, int end)
+        {
+            return value >= start && value <= end;
+        }
+
+        public static bool Between(this DateTime value, DateTime start, DateTime end)
+        {
+            return value >= start && value <= end;
+        }
+    }
+}
