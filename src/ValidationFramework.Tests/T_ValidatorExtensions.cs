17e2dea70e890ae1e908c70cb0184d5a78e405b4
André Kirst <github@andrekirst.de>
2017-12-12 15:35:58 +0100

    #6 - Implementierung von fluent design

---
+++ src/ValidationFramework.Tests/T_ValidatorExtensions.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ValidationFramework.Tests
+{
+    public class T_ValidatorExtensions
+    {
+        [Fact(DisplayName = "TestFluentDesignEnableCaching")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "EnableCaching")]
+        public void TestFluentDesignEnableCaching()
+        {
+            Validator<int> validator = new Validator<int>()
+                .EnableCaching();
+
+            bool expected = true;
+            bool actual = validator.EnableCaching;
+
+            Assert.Equal(expected: expected, actual: actual);
+        }
+
+        [Fact(DisplayName = "TestFluentDesignDisableCaching")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "DisableCaching")]
+        public void TestFluentDesignDisableCaching()
+        {
+            Validator<int> validator = new Validator<int>()
+                .DisableCaching();
+
+            bool expected = false;
+            bool actual = validator.EnableCaching;
+
+            Assert.Equal(expected: expected, actual: actual);
+        }
+
+        [Fact(DisplayName = "TestFluentDesignEnableReturnOnlyErrors")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "EnableReturnOnlyErrors")]
+        public void TestFluentDesignEnableReturnOnlyErrors()
+        {
+            Validator<int> validator = new Validator<int>()
+                .EnableReturnOnlyErrors();
+
+            bool expected = true;
+            bool actual = validator.ReturnOnlyErrors;
+
+            Assert.Equal(expected: expected, actual: actual);
+        }
+
+        [Fact(DisplayName = "TestFluentDesignDisableReturnOnlyErrors")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "DisableReturnOnlyErrors")]
+        public void TestFluentDesignDisableReturnOnlyErrors()
+        {
+            Validator<int> validator = new Validator<int>()
+                .DisableReturnOnlyErrors();
+
+            bool expected = false;
+            bool actual = validator.ReturnOnlyErrors;
+
+            Assert.Equal(expected: expected, actual: actual);
+        }
+
+        [Fact(DisplayName = "TestFluentDesignAdd")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "Add")]
+        public void TestFluentDesignAdd()
+        {
+            Validator<int> validator = new Validator<int>()
+                .Add(new Validation<int>(
+                    messageOnError: "ERR",
+                    messageOnSuccess: "SUC",
+                    name: "MOD",
+                    originalValue: (i) => i,
+                    validationFunction: (i) => i % 2 == 0));
+
+            Assert.Equal(expected: 1, actual: validator.Validations.Count);
+        }
+
+        [Fact(DisplayName = "TestFluentDesignAddIEnumerable")]
+        [Trait(name: "Issue", value: "6")]
+        [Trait(name: "Function", value: "Add")]
+        public void TestFluentDesignAddIEnumerable()
+        {
+            List<AbstractValidation<int>> list = new List<AbstractValidation<int>>();
+            list.Add(new Validation<int>(
+                    messageOnError: "ERR",
+                    messageOnSuccess: "SUC",
+                    name: "MOD",
+                    originalValue: (i) => i,
+                    validationFunction: (i) => i % 2 == 0));
+
+            list.Add(new Validation<int>(
+                    messageOnError: "ERR",
+                    messageOnSuccess: "SUC",
+                    name: "MOD2",
+                    originalValue: (i) => i,
+                    validationFunction: (i) => i + 1 % 2 == 0));
+
+            Validator<int> validator = new Validator<int>()
+                .Add(validations: list);
+
+            Assert.Equal(expected: 2, actual: validator.Validations.Count);
+        }
+    }
+}
---
+++ src/ValidationFramework/ValidatorExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationFramework
+{
+    public static class ValidatorExtensions
+    {
+        public static Validator<T> EnableCaching<T>(this Validator<T> validator)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.EnableCaching = true;
+
+            return validator;
+        }
+
+        public static Validator<T> DisableCaching<T>(this Validator<T> validator)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.EnableCaching = false;
+
+            return validator;
+        }
+
+        public static Validator<T> EnableReturnOnlyErrors<T>(this Validator<T> validator)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.ReturnOnlyErrors = true;
+
+            return validator;
+        }
+
+        public static Validator<T> DisableReturnOnlyErrors<T>(this Validator<T> validator)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.ReturnOnlyErrors = false;
+
+            return validator;
+        }
+
+        public static Validator<T> Add<T>(this Validator<T> validator, AbstractValidation<T> validation)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.AddValidation(validation: validation);
+
+            return validator;
+        }
+
+        public static Validator<T> Add<T>(this Validator<T> validator, IEnumerable<AbstractValidation<T>> validations)
+        {
+            if (validator == null)
+            {
+                throw new InvalidOperationException(message: "Validator is null");
+            }
+
+            validator.AddValidation(validations: validations);
+
+            return validator;
+        }
+    }
+}
