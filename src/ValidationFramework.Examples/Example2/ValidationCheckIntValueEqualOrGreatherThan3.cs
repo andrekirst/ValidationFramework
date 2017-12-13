0a8998ac6b71a29a2f561eb922df7b2c5d850e3e
André Kirst <github@andrekirst.de>
2017-12-13 17:42:08 +0100

    #9 - Examples hinzugefügt

---
+++ src/ValidationFramework.Examples/Example2/ValidationCheckIntValueEqualOrGreatherThan3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValidationFramework.Examples.Example2
+{
+    public class ValidationCheckIntValueEqualOrGreatherThan3 : AbstractValidation<int>
+    {
+        public override string Name => "CHECK_VALUE_EQUAL_OR_GREATHER_THAN_3";
+
+        public override Func<int, bool> ValidationFunction => (i) => i >= 3;
+
+        public override string MessageOnError => "Value is smaller than 3";
+
+        public override Func<int, object> OriginalValue => (i) => i;
+
+        public override string MessageOnSuccess => throw new NotImplementedException();
+    }
+}
