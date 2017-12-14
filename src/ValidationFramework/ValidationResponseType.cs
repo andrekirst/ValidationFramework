bbfe802692da20d4333960b67bfa4d409ee67dbd
André Kirst <github@andrekirst.de>
2017-12-14 13:02:20 +0100

    #9 - Dokumentation und Beispiele

--- src/ValidationFramework/ValidationResponseType.cs
+++ src/ValidationFramework/ValidationResponseType.cs
@@ -1,8 +1,17 @@
 ﻿namespace ValidationFramework
 {
+    /// <summary>
+    /// The type of the response that was validatet
+    /// </summary>
     public enum ValidationResponseType
     {
+        /// <summary>
+        /// If the validation was successful
+        /// </summary>
         Success = 1,
+        /// <summary>
+        /// if the validation occured an error
+        /// </summary>
         Error = 2
     }
 }
